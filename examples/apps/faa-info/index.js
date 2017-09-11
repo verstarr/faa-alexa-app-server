7115c51e24fe5fbc1f752f53e38f7fc1afd84197
Ver Starr <ver.starr@gmail.com>
2017-09-11 01:51:55 -0700

    adding my project faa info

--- examples/apps/faa-info/index.js
+++ examples/apps/faa-info/index.js
@@ -18,5 +18,60 @@ app.launch(function(req, res) {
 
 });
 
+app.intent('airportinfo', {
 
+    'slots': {
+
+            'AIRPORTCODE': 'FAACODES'
+
+        },
+
+        'utterances': ['{|flight|airport} {|delay|status} {|info} {|for} {-|AIRPORTCODE}']
+
+    },
+
+    function(req, res) {
+        //get the slot
+
+        var airportCode = req.slot('AIRPORTCODE');
+
+        var reprompt = 'Tell me an airport code to get delay information.';
+
+        if (_.isEmpty(airportCode)) {
+
+            var prompt = 'I didn\'t hear an airport code. Tell me an airport code.';
+
+            res.say(prompt).reprompt(reprompt).shouldEndSession(false);
+
+            return true;
+
+        } else {
+
+            var faaHelper = new FAADataHelper();
+
+            faaHelper.requestAirportStatus(airportCode).then(function(airportStatus) {
+
+                console.log(airportStatus);
+
+                res.say(faaHelper.formatAirportStatus(airportStatus)).send();
+
+            }).catch(function(err) {
+
+                console.log(err.statusCode);
+
+                var prompt = 'I didn\'t have data for an airport code of ' + airportCode;
+
+                res.say(prompt).reprompt(reprompt).shouldEndSession(false).send();
+
+            });
+
+            return false;
+
+        }
+
+    }
+
+);
+
+console.log(app.utterances().replace(/\{\-\|/g, '{'));
 module.exports = app;
